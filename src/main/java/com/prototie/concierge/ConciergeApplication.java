a217f7c4cfe5c8c5761ac01f044c5d476464b3e2
Lee sung joo <sjlee@prototie.net>
2026-02-13 10:17:37 +0900

    initial commit

---
+++ src/main/java/com/prototie/concierge/ConciergeApplication.java
@@ -0,0 +1,13 @@
+package com.prototie.concierge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConciergeApplication.class, args);
+	}
+
+}
